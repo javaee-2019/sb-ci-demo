b94dfb9dfa30ba018eb9fbff42103b6d11b11fe4
shanmu2020 <854731306@qq.com>
2021-01-31 21:57:07 +0800

    1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,19 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -19,12 +22,15 @@ public class DemoApplication {
 
     @RequestMapping("/")
     public Object start() {
-        return hello();
+        return "Hello World";
     }
 
     @RequestMapping("/hello")
-    public Object hello() {
-        return getUrl() + "====>" + new Date().toLocaleString();
+    public Object hello(HttpServletRequest request) {
+        String url = getUrl();
+        String ipAddress = getIPAddress(request);
+        log.info("{}==>{}", ipAddress, url);
+        return ipAddress + "===>" + url + "====>" + new Date().toLocaleString();
     }
 
     @RequestMapping("/time")
@@ -41,5 +47,21 @@ public class DemoApplication {
             e.printStackTrace();
         }
         return "http://" + address.getHostAddress();
+    }
+
+    public String getIPAddress(HttpServletRequest request) {
+        try {
+            String remoteAddress = "";
+            if (request != null) {
+                remoteAddress = request.getHeader("X-Forwarded-For");
+                if (remoteAddress == null || "".equals(remoteAddress)) {
+                    remoteAddress = request.getRemoteAddr();
+                }
+            }
+            remoteAddress = remoteAddress != null && remoteAddress.contains(",") ? remoteAddress.split(",")[0] : remoteAddress;
+            return remoteAddress;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
