048d3485abeaf748b199ee9806df0fbc78abe1f5
wen.he <wen.he@timanetworks.com>
2021-05-08 17:56:57 +0800

    1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.util.IpUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.HashMap;
 
 @SpringBootApplication
 @RestController
@@ -16,10 +19,12 @@ public class DemoApplication {
     }
 
     @RequestMapping("/hello")
-    public Object hello() {
-        Integer num = 10;
-        num++;
-        return "hello " + num;
+    public Object hello(String name) {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("name", name);
+        map.put("ip", IpUtil.getLocalIpByNetcard());
+        map.put("time", new Date().toLocaleString());
+        return map;
     }
 
     @RequestMapping("/time")
--- src/test/java/com/example/demo/WSTest.java
+++ src/test/java/com/example/demo/WSTest.java
@@ -1,17 +1,25 @@
 package com.example.demo;
 
-import com.example.demo.webservice.client.MESTSP;
-import com.example.demo.webservice.client.MESTSPSoap;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class WSTest {
     @Test
-    public void test5() {
-        MESTSP mestsp = new MESTSP();
-        MESTSPSoap mestspSoap = mestsp.getMESTSPSoap();
-        String data = "[{\"vin\":\"SGHJ9876545676546\",\"CheckRsult\":\"Y\",\"CheckTime\":\"2020-10-10 10:10:10\"}]";
-//        String data = "{\"vin\":\"SGHJ9876545676546\",\"CheckRsult\":\"Y\",\"CheckTime\":\"2020-10-10 10:10:10\"}";
-        String tboxData = mestspSoap.getTboxData(data);
-        System.out.println("tboxData = " + tboxData);
+    public void test5() throws InterruptedException {
+        System.out.println(" sleep . . . .");
+        Thread.sleep(10000);
+        HashMap<Object, Object> map = new HashMap<>();
+        while (true) {
+            String str = UUID.randomUUID().toString() + new Date().toLocaleString();
+            map.put(UUID.randomUUID().toString(), str);
+            System.out.println("map = " + map.size());
+            if (map.size() % 100000 == 0) {
+                System.out.println(" sleep . . . .");
+                Thread.sleep(1000);
+            }
+        }
     }
 }
